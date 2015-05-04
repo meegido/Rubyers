744f595b12851a1ad15c63cc90d323f5f4d912a4
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-05-04 17:58:03 +0200

    friendship model

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CommentsControllerTest < ActionController::TestCase
+class FriendshipTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
